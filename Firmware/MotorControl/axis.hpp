904fdf61c3bc0349f5e5f245f6939a6abd023b06
zoe <2983682534@qq.com>
2026-06-17 21:47:28 +0800

    change heartbeat rate

--- Firmware/MotorControl/axis.hpp
+++ Firmware/MotorControl/axis.hpp
@@ -50,7 +50,7 @@ public:
     struct CANConfig_t {
         uint32_t node_id = 0;
         bool is_extended = false;
-        uint32_t heartbeat_rate_ms = 100;
+        uint32_t heartbeat_rate_ms = 5;
         uint32_t encoder_rate_ms = 0;
         uint32_t motor_error_rate_ms = 0;
         uint32_t encoder_error_rate_ms = 0;
